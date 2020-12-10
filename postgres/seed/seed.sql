957a7c68439a0ba5447df2eeab0a4e6e3b6288a8
Marco F <marco.friso@gmail.com>
2020-12-10 11:44:18 +0100

    feat: implement Docker container

---
+++ postgres/seed/seed.sql
@@ -0,0 +1,9 @@
+-- Seed data with a fake user for testing
+BEGIN TRANSACTION;
+
+INSERT into users (name, email, entries, joined) values ('Jessie', 'jessie@gmail.com', 5, '2020-01-01');
+
+-- password: see JESSIE_PASSWORD in .env file
+INSERT into login (hash, email) values ('$2a$10$WAK21U0LWl7C//jJ.DOB2uPP1DJQh7KUDgasdyQeGzkop2Pzl8W7u', 'jessie@gmail.com');
+
+COMMIT;
--- server.js
+++ server.js
@@ -3,31 +3,40 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
+const morgan = require("morgan");
 
 const register = require("./controllers/register");
 const signin = require("./controllers/signin");
 const profile = require("./controllers/profile");
 const image = require("./controllers/image");
 
+// connect to your own database here
 const db = knex({
-  // connect to your own database here
   client: "pg",
-  connection: {
-    host: "127.0.0.1",
-    user: "",
-    password: "",
-    database: "smart-brain",
-  },
+  connection: process.env.POSTGRES_URI,
 });
 
 const app = express();
 
-app.use(cors());
+const whitelist = ["http://localhost:3001"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(morgan("combined"));
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send(db.users);
+  res.send("MAKE YOURSELF GREAT AGAIN");
 });
+
 app.post("/signin", signin.handleSignin(db, bcrypt));
 app.post("/register", (req, res) => {
   register.handleRegister(req, res, db, bcrypt);
